99a934c79520211e42dfc5dc860e93f37e83ef5c
System Administrator <root@n175-33-1-159.meb2.vic.optusnet.com.au>
2019-05-30 11:49:42 -0500

    Changed port

--- index.js
+++ index.js
@@ -62,4 +62,4 @@ app.use('/graphql', express_graphql({
     graphiql: true
 }));
 
-app.listen(3000, () => console.log('listening on port 3000'));
+app.listen(4000, () => console.log('listening on port 4000'));
